d56925f4151cd4546f56cabbc4249e015faaec8c
Wenyao <peterleewenyaoalpha@gmail.com>
2020-05-28 13:20:25 +0800

    add user module

---
+++ backend/components/user/userController.js
@@ -0,0 +1,71 @@
+const bcrypt = require('bcrypt');
+const { users } = require('./users');
+
+/**
+ * POST create a new user
+ * /api/user/signup
+ */
+module.exports.signUp = async (req, res) => {
+    console.log('req.body:', req.body);
+    const { email, password } = req.body;
+
+    if (!email && !password) {
+        res.status(400).json({ message: `Email and password is not provided` });
+    } else if (!email) {
+        res.status(400).json({ message: `Email is not provided` });
+    } else if (!password) {
+        res.status(400).json({ message: `Password is not provided` });
+    }
+
+    const result = users.findIndex((user) => user.email === email);
+
+    if (result !== -1) {
+        res.status(403).json({ message: `User already exists!` });
+    }
+
+    try {
+        const hashedPw = await bcrypt.hash(password, 10);
+
+        const newUser = {
+            id: users.length + 1,
+            email,
+            password: hashedPw,
+        };
+
+        users.push(newUser);
+
+        res.status(201).json({ message: 'New User created!' });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+};
+
+/**
+ * POST user login
+ * /api/user/login
+ */
+module.exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = users.find((user) => user.email === email);
+
+        if (!user) {
+            return res.status(401).json({ message: 'Auth failed' });
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) {
+            return res.status(401).json({ message: 'Auth failed' });
+        }
+
+        const token = jwt.sign({ email: user.email, userId: user._id }, 'secret', {
+            expiresIn: '1h',
+        });
+
+        res.status(200).json({ token });
+    } catch (error) {
+        return res.status(401).json({ message: 'Auth failed' });
+    }
+};
---
+++ backend/components/user/users.js
@@ -0,0 +1,11 @@
+module.exports.users = [
+    {
+        email: 'john@mail.com',
+        password: '$2b$10$IEdBievEt3LTQEOgTqYid.lQGXieTO.6YViUAq8BT//vT0e4UG0cW',
+    },
+    {
+        email: 'dave@mail.com',
+        password: '$2b$10$IEdBievEt3LTQEOgTqYid.lQGXieTO.6YViUAq8BT//vT0e4UG0cW',
+        isAdmin: true,
+    },
+];
