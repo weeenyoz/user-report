f4b0785b0fc1009211bf91ca6b87c08d45f23f8b
Wenyao <peterleewenyaoalpha@gmail.com>
2020-05-29 09:50:11 +0800

    add overlooked isAdmin property in token

--- backend/components/user/userController.js
+++ backend/components/user/userController.js
@@ -46,7 +46,7 @@ module.exports.signUp = async (req, res) => {
  * /api/user/login
  */
 module.exports.login = async (req, res, next) => {
-    const { email, password } = req.body;
+    const { email, password, isAdmin } = req.body;
 
     try {
         const user = users.find((user) => user.email === email);
@@ -61,9 +61,13 @@ module.exports.login = async (req, res, next) => {
             return res.status(401).json({ message: 'Auth failed' });
         }
 
-        const token = jwt.sign({ email: user.email, userId: user.id }, 'secret', {
-            expiresIn: '1h',
-        });
+        const token = jwt.sign(
+            { email: user.email, userId: user.id, isAdmin },
+            'secret',
+            {
+                expiresIn: '1h',
+            },
+        );
 
         res.status(200).json({ token, expiresIn: 36000 });
     } catch (error) {
--- backend/components/user/users.js
+++ backend/components/user/users.js
@@ -3,6 +3,7 @@ module.exports.users = [
         id: 1,
         email: 'john@mail.com',
         password: '$2b$10$IEdBievEt3LTQEOgTqYid.lQGXieTO.6YViUAq8BT//vT0e4UG0cW',
+        isAdmin: false,
     },
     {
         id: 2,
